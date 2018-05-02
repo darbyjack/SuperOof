ec51f499e8dc757ffdd8c6f1e192a90baef61efb
darbyjack <admin@glaremasters.me>
2018-05-02 18:11:09 -0500

    Initial Commit

---
+++ src/main/java/me/glaremasters/superoof/SuperOof.java
@@ -0,0 +1,17 @@
+package me.glaremasters.superoof;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SuperOof extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
