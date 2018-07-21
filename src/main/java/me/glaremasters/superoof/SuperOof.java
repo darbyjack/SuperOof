6de0dcc8136ed6926ae44c0b30b5a101386ebf23
darbyjack <admin@glaremasters.me>
2018-07-21 17:38:18 -0500

    Implemented UpdateChecker

--- src/main/java/me/glaremasters/superoof/SuperOof.java
+++ src/main/java/me/glaremasters/superoof/SuperOof.java
@@ -1,6 +1,7 @@
 package me.glaremasters.superoof;
 
 import me.glaremasters.superoof.events.Chat;
+import me.glaremasters.superoof.updater.SpigotUpdater;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SuperOof extends JavaPlugin {
@@ -10,11 +11,24 @@ public final class SuperOof extends JavaPlugin {
         // Plugin startup logic
 
         getServer().getPluginManager().registerEvents(new Chat(), this);
+        SpigotUpdater updater = new SpigotUpdater(this, 58916);
+        updateCheck(updater);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void updateCheck(SpigotUpdater updater) {
+        try {
+            if (updater.checkForUpdates()) {
+                getLogger().info("You appear to be running a version other than our latest stable release." + " You can download our newest version at: " + updater.getResourceURL());
+            }
+        } catch (Exception ex) {
+            getLogger().info("Could not check for updates! Stacktrace:");
+            ex.printStackTrace();
+        }
     }
 }
