ab5b779fbb00c3776b5f17476f23e4b03ad02e4c
darbyjack <admin@glaremasters.me>
2018-10-12 10:44:00 -0500

    Deleted stupid stuff

--- src/main/java/me/glaremasters/superoof/SuperOof.java
+++ src/main/java/me/glaremasters/superoof/SuperOof.java
@@ -1,16 +1,10 @@
 package me.glaremasters.superoof;
 
-import me.glaremasters.superoof.commands.CommandHelp;
-import me.glaremasters.superoof.commands.base.CommandHandler;
 import me.glaremasters.superoof.events.Chat;
 import me.glaremasters.superoof.updater.SpigotUpdater;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.stream.Stream;
-
 public final class SuperOof extends JavaPlugin {
-
-    private CommandHandler commandHandler;
 
     @Override
     public void onEnable() {
@@ -19,13 +13,6 @@ public final class SuperOof extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new Chat(this), this);
         SpigotUpdater updater = new SpigotUpdater(this, 58916);
         updateCheck(updater);
-
-        commandHandler = new CommandHandler();
-        commandHandler.enable();
-
-        getCommand("so").setExecutor(commandHandler);
-
-        Stream.of(new CommandHelp(this)).forEach(commandHandler::register);
 
     }
 
@@ -43,13 +30,5 @@ public final class SuperOof extends JavaPlugin {
             getLogger().info("Could not check for updates! Stacktrace:");
             ex.printStackTrace();
         }
-    }
-
-    /**
-     * Get the command handler in the plugin
-     * @return the handler being used
-     */
-    public CommandHandler getCommandHandler() {
-        return commandHandler;
     }
 }
